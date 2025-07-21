ed4ada3965e35a51b874aa5570143748fd77401e
Cesar Cedillo <cesar.cedillo@wizeline.com>
2025-07-21 09:10:42 +0200

    adding infrastruture test to Auth micro

--- Backend/Ticketing.Auth/src/Ticketing.Auth.Infrastructure/Data/Repositories/UserRepository.cs
+++ Backend/Ticketing.Auth/src/Ticketing.Auth.Infrastructure/Data/Repositories/UserRepository.cs
@@ -1,25 +1,26 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Ticketing.Auth.Domain.Aggregates;
 using Ticketing.Auth.Domain.Interfaces;
+using Ticketing.Core.Infrastructure.EntityFramework.Repositories;
 
 namespace Ticketing.Auth.Infrastructure.Data.Repositories;
-public class UserRepository : IUserRepository
+public class UserRepository : GenericRepository<User>, IUserRepository
 {
   private readonly AuthDbContext _dbContext;
 
-  public UserRepository(AuthDbContext dbContext)
+  public UserRepository(AuthDbContext dbContext) : base(dbContext)
   {
     _dbContext = dbContext;
   }
 
-  public async Task<User?> GetByUserNameAsync(string userName, CancellationToken cancellationToken)
+  public async Task<User?> GetByUserNameAsync(string userName, CancellationToken cancellationToken = default)
   {
     return await _dbContext.Users
         .AsNoTracking()
         .FirstOrDefaultAsync(u => u.UserName == userName, cancellationToken);
   }
 
-  public async Task AddAsync(User user, CancellationToken cancellationToken)
+  public async Task AddAsync(User user, CancellationToken cancellationToken = default)
   {
     _dbContext.Users.Add(user);
     await _dbContext.SaveChangesAsync(cancellationToken);
---
+++ Backend/Ticketing.Auth/test/Ticketing.Auth.Infrastructure.Tests/UserRepositoryTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Ticketing.Auth.Domain.Aggregates;
+using Ticketing.Auth.Domain.Enum;
+using Ticketing.Auth.Infrastructure.Data;
+using Ticketing.Auth.Infrastructure.Data.Repositories;
+
+namespace Ticketing.Auth.Infrastructure.Tests;
+
+public class UserRepositoryTests
+{
+  private readonly AuthDbContext _context;
+
+  public UserRepositoryTests()
+  {
+    var contextOptions = new DbContextOptionsBuilder<AuthDbContext>()
+        .UseInMemoryDatabase(Guid.NewGuid().ToString())
+        .Options;
+    _context = new AuthDbContext(contextOptions);
+
+    _context.Users.RemoveRange(_context.Users);
+    _context.SaveChanges();
+
+    var user1 = new User("alice", "1234", Role.Customer);
+    var user2 = new User("bob", "1234", Role.Agent);
+
+    _context.Users.AddRange(user1, user2);
+    _context.SaveChanges();
+  }
+
+  [Fact]
+  public async Task GetByUserNameAsync_Should_Return_User_When_Exists()
+  {
+    var repo = new UserRepository(_context);
+
+    var user = await _context.Users.FirstAsync();
+
+    var result = await repo.GetByUserNameAsync(user.UserName);
+
+    result.Should().NotBeNull();
+    result!.UserName.Should().Be(user.UserName);
+  }
+
+  [Fact]
+  public async Task GetByUserNameAsync_Should_Return_Null_When_Not_Found()
+  {
+    var repo = new UserRepository(_context);
+
+    var result = await repo.GetByUserNameAsync("nonExistentUser");
+
+    result.Should().BeNull();
+  }
+}
